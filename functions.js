a8787fd03e7602dbc3b0f36cb8029ac15c4b254f
Serhii <skyiashko@gmail.com>
2020-03-31 23:00:38 +0300

    add description and date

--- functions.js
+++ functions.js
@@ -1,40 +1,48 @@
 function addTask(id, task) {
-    const $taskItem = $('<li>').addClass('list-group-item').text(task.title);
+    const $taskItem = $('<li>')
+        .addClass('list-group-item')
+        .text(task.title || 'Untitled')
+        .attr('data-id', id);
 
+    const $btnEdit = $('<button>')
+            .addClass('btn btn-warning btn-xs btn-edit pull-right')
+            .html('<i class="glyphicon glyphicon-pencil"></i>')
+            .css('margin-left', '10px');
+
+    const $btnDelete = $('<button>')
+            .addClass('btn btn-danger btn-xs pull-right btn-delete')
+            .html('<i class="glyphicon glyphicon-trash"></i>')
+            .css('margin-left', '10px');
+
+    const $btnInfo = $('<button>')
+            .addClass('btn btn-primary btn-xs btn-info pull-right')
+            .html('<i class="glyphicon glyphicon-tasks"></i>')
+            .attr('data-toggle', 'collapse')
+            .attr('data-target', '#info' + id);
+
+    $taskItem
+            .append($btnDelete)
+            .append($btnEdit)
+            .append($btnInfo);
+    
+    const $dt = $('<dt>')
+        .text(task.date || 'No date');
+
+    const $dd = $('<dd>')
+        .text(task.description || 'No description');
+    
+    const $dl = $('<dl>')
+        .append($dt)
+        .append($dd);
+
+    const $infoContainer = $('<div>')
+        .addClass('collapse container')
+        .attr('id', 'info' + id)
+        .append($dl);
+
+    $taskItem.append($infoContainer); 
+     
     $tasksList
         .find(`[data-status="${task.status}"]`)
         .append($taskItem);
-
-    countStatistics();
-}
-
-function removeAllTask () {    
-        localStorage.clear();
-        $('[data-status]').find('li').remove(); 
-          
-        countStatistics();
-    }
-
-function countStatistics() {
-     let taskCounts = {
-        1: 0,
-        2: 0,
-        3: 0
-     }; 
-
-     for (let key in localStorage) {
-        if (localStorage.hasOwnProperty(key)) {
-            const task = JSON.parse(localStorage[key]);
-
-            taskCounts[task.status]++;
-        }
-     }
-     
-     for (let key in taskCounts) {
-        let $taskCounter = $(`[data-count-status="${key}"]`);
-
-        if($taskCounter) {
-             $taskCounter.text(taskCounts[key]);
-        }
-     }
 }
--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover();
+});
+
 const $formAddTask = $('#formAddTask');
 const $tasksList = $('#tasks-list');
 const $modalAddTask = $('#modalAddTask');
+const $formEditTask = $('#formEditTask');
+const $modalEditTask = $('#modalEditTask');
 
 $formAddTask.on('submit', function(event) {
     event.preventDefault();
@@ -8,7 +14,9 @@ $formAddTask.on('submit', function(event) {
     let task = {
         id: new Date().getTime(), 
         title: $('[name="title"]', this).val(),
-        status: 1 //1 - todo, 2 - in progress, 3 - done
+        status: 1, //1 - todo, 2 - in progress, 3 - done
+        date: $('[name="date"]', this).val(),
+		description: $('[name="description"]', this).val(),
     };
 
     addTask(task.id, task);
@@ -17,9 +25,7 @@ $formAddTask.on('submit', function(event) {
 
     $modalAddTask.modal('hide');
 
-    this.reset(); //метод у формы, чтобы сбросить все значения полей по умолчанию
-    
-    countStatistics();
+    this.reset(); 
 });
 
 for (let key in localStorage) {
@@ -29,10 +35,48 @@ for (let key in localStorage) {
     const task = JSON.parse(localStorage[key]);
 
     addTask(task.id, task);
-
-    countStatistics();
 }
 
-const $removeBtn = $('#removealltask');
+$('body').on('click', '.btn-edit', function() {
+    const $parent = $(this).parents('[data-id]');
 
-$removeBtn.on('click',  removeAllTask);
+    const taskId = $parent.data('id');
+
+    const task = JSON.parse(localStorage.getItem(taskId));
+
+    for(let key in task) {
+        $formEditTask.find(`[name="${key}"]`).val(task[key]);
+    }
+
+    $modalEditTask.modal('show');
+});
+
+$formEditTask.on('submit', function(event) {
+    event.preventDefault();
+
+    let task = { 
+        title: $('[name="title"]', this).val(),
+        status: $('[name="status"]', this).val(),  //1 - todo, 2 - in progress, 3 - done
+        id: $('[name="id"]', this).val(),
+        date: $('[name="date"]', this).val(),
+		description: $('[name="description"]', this).val()
+    };
+
+    $tasksList.find(`[data-id="${task.id}"]`).remove();
+
+    addTask(task.id, task);
+
+    $modalEditTask.modal('hide');
+
+    localStorage.setItem(task.id, JSON.stringify(task));
+});
+
+$('body').on('click', '.btn-delete', function(event) {
+
+    const $parent = $(this).parents('[data-id]')
+
+    const taskId = $parent.data('id');
+    $parent.remove();
+
+    localStorage.removeItem(taskId);
+});
